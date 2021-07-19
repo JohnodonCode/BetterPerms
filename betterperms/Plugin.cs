832eff470e49a37f77f97088338db4b5795ee591
John Bostick <jbost0114@gmail.com>
2021-07-19 11:11:20 -0400

    Update version number of development branch

--- betterperms/Plugin.cs
+++ betterperms/Plugin.cs
@@ -10,7 +10,7 @@ namespace betterperms
         public override string Author { get; } = "Johnodon";
         public override string Name { get; } = "BetterPerms";
         public override string Prefix { get; } = "BP";
-        public override Version Version { get; } = new Version(1, 0, 0);
+        public override Version Version { get; } = new Version(1, 0, 1);
         public override Version RequiredExiledVersion { get; } = new Version(2, 11, 1);
     }
 }
