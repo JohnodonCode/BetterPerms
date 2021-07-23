6479c5e029ce388ca2540005860a33773ca97d5a
John Bostick <jbost0114@gmail.com>
2021-07-23 00:22:48 -0400

    update version

--- betterperms/Plugin.cs
+++ betterperms/Plugin.cs
@@ -11,7 +11,7 @@ namespace BetterPerms
         public override string Author { get; } = "Johnodon";
         public override string Name { get; } = "BetterPerms";
         public override string Prefix { get; } = "BetterPerms";
-        public override Version Version { get; } = new Version(1, 0, 1);
+        public override Version Version { get; } = new Version(1, 1, 1);
         public override Version RequiredExiledVersion { get; } = new Version(2, 11, 1);
 
         public override void OnEnabled()
